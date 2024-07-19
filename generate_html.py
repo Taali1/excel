30c348f11bcc53da1b637181179f3b1292f2d84b
Taali <matrebacz125@gmail.com>
2024-07-19 19:29:01 +0200

    a

--- generate_html.py
+++ generate_html.py
@@ -18,13 +18,19 @@ def create_page(products: dict, title: str) -> str:
 
 
         gross_price = float(product['price'])
+
         netto_price = round((gross_price*.8130081300813008), 2)
+        netto_price_str = str(netto_price)
+
         off_price = round((netto_price*.75), 2)
         off_price_str = str(off_price)
 
         if len(off_price_str.split('.')[1]) == 1:
             off_price_str += '0'
         
+        if len(netto_price_str.split('.')[1]) == 1:
+            netto_price_str += '0'
+
 
         try: 
             result_div += generate_div(
@@ -37,7 +43,7 @@ def create_page(products: dict, title: str) -> str:
                 name_data.get('width') or '<i>Null</i>',
                 name_data.get('height') or '<i>Null</i>', 
                 
-                netto_price, 
+                netto_price_str, 
                 off_price_str
                 )
         except Exception as error:
